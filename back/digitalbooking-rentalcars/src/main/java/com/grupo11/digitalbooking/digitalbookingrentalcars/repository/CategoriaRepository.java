5c3f8f9e6193e86c19a08d558392420ba2b62ff2
rfescobar <72085207+rfescobar@users.noreply.github.com>
2022-10-24 16:25:38 -0300

    CRUD categorías

---
+++ back/digitalbooking-rentalcars/src/main/java/com/grupo11/digitalbooking/digitalbookingrentalcars/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.grupo11.digitalbooking.digitalbookingrentalcars.repository;
+
+import com.grupo11.digitalbooking.digitalbookingrentalcars.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
