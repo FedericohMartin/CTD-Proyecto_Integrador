7f32c77e567a081c9013de20b89dcabb3432d73d
Sabrina Giuliana Incinga <sabrinagincinga@gmail.com>
2022-10-26 11:31:20 +0000

    Booking-GR11-1: Implementar template general responsive

--- front/booking/src/App.jsx
+++ front/booking/src/App.jsx
@@ -1,16 +1,39 @@
 import './App.css';
 import Home from './components/Home';
+import Header from './components/tools/Header';
+import Footer from './components/tools/Footer';
+import homeStyles from './styles/home.module.css'
+import Login from './components/Login';
+import Menu from './components/tools/Menu';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
+  const [hideMenu, setHideMenu] = useState(true);
+
+  const onMenuClicked = () => {
+    setHideMenu(false);
+  }
+
+  const onCloseClicked = () => {
+    setHideMenu(true);
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home user={""}/>}> </Route>
-        </Routes>
-      </BrowserRouter>
+      
+        <BrowserRouter>
+          <Header user={""} onMenuParentClicked={onMenuClicked}></Header>
+          <div onClick={onCloseClicked} className={`${homeStyles.opacity} ${!hideMenu && homeStyles.darken}`}></div>  
+          <Menu onParentCloseClicked={onCloseClicked} show={hideMenu}></Menu>
+          <Routes>
+            <Route path="/" element={<Home onGParentCloseClicked= {onCloseClicked} show={hideMenu}/>}> </Route>
+            <Route path='/login' element={<Login/>}></Route>
+          </Routes>
+          <Footer></Footer>
+        </BrowserRouter>
+      
     </div>
   );
 }
--- front/booking/src/components/Home.jsx
+++ front/booking/src/components/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import styles from '../styles/home.module.css'
-import Header from "./tools/Header";
 import Body from "./tools/Body";
-import Footer from "./tools/Footer";
 
-
-function Home(props){
+function Home(){
 
     return(
         <>
-            <div className={styles.opacity}></div>
-            <Header user={props.user}></Header>
             <Body></Body>
-            <Footer></Footer>
         </>
     )
 }
