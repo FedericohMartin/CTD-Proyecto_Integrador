bea0d172c153246bd3a7de5bea2e270591b2f0c9
FedericohMartin <federicohmartin@gmail.com>
2022-11-01 23:28:59 -0300

    RESET

--- db/script.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `rentcarsdigitalbookingprod`.`categorias` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NULL DEFAULT NULL,
-  `titulo` VARCHAR(255) NULL DEFAULT NULL,
-  `url_img` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
