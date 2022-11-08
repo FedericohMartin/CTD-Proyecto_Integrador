9ce2e9eb9fe43a78d244f1e29172fbcee5e1cba7
rfescobar <72085207+rfescobar@users.noreply.github.com>
2022-11-07 23:45:26 -0300

    Se agrega relación entre ciudad y producto

---
+++ back/digitalbooking-rentalcars/src/main/java/com/grupo11/digitalbooking/digitalbookingrentalcars/model/dto/ProductDTO.java
@@ -0,0 +1,25 @@
+package com.grupo11.digitalbooking.digitalbookingrentalcars.model.dto;
+
+import com.grupo11.digitalbooking.digitalbookingrentalcars.model.Category;
+import com.grupo11.digitalbooking.digitalbookingrentalcars.model.City;
+import com.grupo11.digitalbooking.digitalbookingrentalcars.model.ProductFeature;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductDTO {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Boolean carry_on;
+    private Boolean suitcase;
+    private City city;
+    private Category category;
+    private List<ProductFeature> features = new ArrayList<>();
+
+}
