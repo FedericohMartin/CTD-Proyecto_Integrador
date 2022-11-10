51b901773594f9746377cfe2c06c22465e035c63
Adriana Denise Fernandez <fernandez.adrianadenise@gmail.com>
2022-11-10 01:50:40 -0300

    para arreglar

--- front/booking/src/components/tools/Searchbox.jsx
+++ front/booking/src/components/tools/Searchbox.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/searchbox.module.css"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Calendar from "./CalendarSearch";
-import {argCities} from "../../data/cities"
+import citiesService from "../../services/citiesService"; 
 
 function Searchbox(){
+    const [cities, setCities] = useState([]);
 
+    const cityMapper = (cities) => (
+        <div
+            key={`city-${cities.id_cities}`}
+            name={cities.name}
+            country={cities.country}
+            state={cities.state}
+        />)
+
+        useEffect(() => {
+            citiesService
+            .getAll()
+            .then((response) => {
+                setCities(response.data);
+            })
+    
+        }, []);
+
+    
     return(
         <div className={styles.searchbox}>
             <h1>Busca ofertas en Automoviles</h1>
@@ -14,13 +33,12 @@ function Searchbox(){
             <form id="form" className={styles.container}>
                 <div className={styles.formCities}>
                     <select className={styles.inputSearch} id="cities">
+                        {cities.map(cityMapper)}
                         <option id="title" className={styles.selected} value='null'>
                             ¿En cuál ciudad querés pistear?
                         </option>
 
-                        {argCities.map(city => 
-                            <option value={city.name}>{city.name} - {city.code}</option>
-                        )}
+                        {cities.map(cityMapper)}
 
                     </select>
                 </div>
