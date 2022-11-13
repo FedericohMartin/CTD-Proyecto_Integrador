e3ce5dd2e75fc19ea9dfe601b8869a2398f2fec4
Sabrina-Incinga <sabrinagincinga@gmail.com>
2022-11-13 20:40:11 -0300

    pagination added

--- front/booking/src/components/tools/Categories.jsx
+++ front/booking/src/components/tools/Categories.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
 import style from "../../styles/categories.module.css";
 import Card from "./Card";
 
+function Pagination({onParentPrevClicked, onParentNextClicked, page, buttonContainer, btn, active, onParentNumberClicked}){
+    const onLocalPrevClicked = () => {
+        onParentPrevClicked();
+    }
+    const onLocalNextClicked = () => {
+        onParentNextClicked();
+    }
+
+    const onLocalNumberClicked = (e) => {
+        onParentNumberClicked(e);
+    }
+
+    const pageNumberArray = Array.from({length: page.totalPages}, (v, i) => i+1);
+    const pageNumbreComponents = pageNumberArray.map(n => <div className={n === page.pageNumber+1 && active} onClick={onLocalNumberClicked} data-page={n-1}>{n}</div>)
+
+    return(
+        <>
+            <div className={buttonContainer}>
+                <button className={btn} type="button" onClick={onLocalPrevClicked} disabled={page.pageNumber === 0}>{"<"}</button>
+                {pageNumbreComponents}
+                <button className={btn} type="button" onClick={onLocalNextClicked} disabled={page.totalPages-1 === page.pageNumber}>{">"}</button>
+            </div>  
+        </>
+    )
+}
 
 function Categories(props) {
+    const [pageComponents, setPageComponents] = useState([]);
+    const [page, setPage] = useState({
+        pageNumber: 0,
+        totalPages: Math.trunc(17/4+1),
+    });
+
 
     const categoryMapper = (category) => (
         <div
@@ -28,7 +60,37 @@ function Categories(props) {
             description={product.description}
         />)
 
+    const onNextClicked = () => {
+        setPage(prevState => {
+            const update = {...prevState};
+            update.pageNumber++
 
+            return update;
+        })
+    }
+
+    const onPrevClicked = () => {
+        setPage(prevState => {
+            const update = {...prevState};
+            update.pageNumber--
+
+            return update;
+        })
+    }
+
+    const onNumberClicked= (e) => {
+        const targetPage = e.currentTarget.dataset.page;
+        setPage(prevState => {
+            const update = {...prevState};
+            update.pageNumber = parseInt(targetPage);
+
+            return update;
+        })
+    }
+
+    useEffect(() => {
+        setPageComponents(props.products.map(productMapper))
+    }, [props.products])
 
     return (
         <>
@@ -44,9 +106,19 @@ function Categories(props) {
                 <div className={style.subtitle}>
                     <h2>Recomendaciones</h2>
                 </div>
-                <div className={style.list}>
-                    {props.products.length !== 0 ? props.products.map(productMapper) : <div>No se encontraron productos</div>}
-                </div>
+                <Pagination 
+                    onParentPrevClicked={onPrevClicked} 
+                    onParentNextClicked={onNextClicked} 
+                    page={page}
+                    buttonContainer = {style.buttonContainer}
+                    btn = {style.pageBtn}
+                    active = {style.active}
+                    onParentNumberClicked={onNumberClicked}
+                    />
+                    <div className={style.list}>
+                        {pageComponents.length !== 0 ? pageComponents.slice(page.pageNumber*4, 4*(page.pageNumber+1)) : <div>No se encontraron productos</div>}
+                    </div>
+                  
             </div>
         </>
     );
