b2ea7afd775f35277087de5376e7eb33e70d379e
rfescobar <72085207+rfescobar@users.noreply.github.com>
2022-11-25 15:18:07 -0300

    Issue security

--- back/digitalbooking-rentalcars/src/main/java/com/grupo11/digitalbooking/digitalbookingrentalcars/repository/BookingRepository.java
+++ back/digitalbooking-rentalcars/src/main/java/com/grupo11/digitalbooking/digitalbookingrentalcars/repository/BookingRepository.java
@@ -1,7 +1,10 @@
 package com.grupo11.digitalbooking.digitalbookingrentalcars.repository;
 
 import com.grupo11.digitalbooking.digitalbookingrentalcars.model.Booking;
+import com.grupo11.digitalbooking.digitalbookingrentalcars.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +14,12 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     List<Booking> findByUserId(Integer users_id);
     List<Booking> findByProductId(Integer products_id);
+
+    //Ticket Nº 55
+    @Query("SELECT b FROM Booking b WHERE b.booking.name = :initialDate")//JPQL
+    List<Product>findByInitialDate(@Param("initialDate") String initialDate);
+
+    @Query("SELECT b FROM Booking b WHERE b.booking.name = :finalDate")//JPQL
+    List<Product>findByFinalDate(@Param("finalDate") String finalDate);
+
 }
